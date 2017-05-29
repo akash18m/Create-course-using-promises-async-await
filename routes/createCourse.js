a2476fcb874f6a2e896018beae70302cb18db0c9
Akash <akash18modi@gmail.com>
2017-05-30 02:55:12 +0530

    first commit

--- routes/createCourse.js
+++ routes/createCourse.js
@@ -62,32 +62,23 @@ router.post('/addSection',function(req,res,next)
 });
 
 
-router.post('/addLesson',function(req,res,next)
+router.post('/addLesson',async function(req,res,next)
 {
 	var lessons = req.body.lessons;
 	var section_id = req.body.section_id;
 	var course_id;
-	add_lessons(lessons,section_id,0)
-	.then(function(arr){
-		DB.section.findById(section_id,function(err,data){
-			course_id = data.course_id;
-			update_c_duration([arr[0] , course_id])
-		})
-		.catch(err => {
-			console.log(err)
-		});;
-		res.json(null);
+	var arr = await add_lessons(lessons,section_id,0)
+	DB.section.findById(section_id,function(err,data){
+		course_id = data.course_id;
+		update_c_duration([arr[0] , course_id])
 	})
-	.catch(err => {
-		console.log(err)
-	});;
-
+	res.json(null);	
 });
 
-router.delete('/delete/:course_id',function(req,res,next)
+router.delete('/delete/:course_id',async function(req,res,next)
 {
 	var course_ID = req.params.course_id;
-	DB.section.find({course_id : course_ID},function(err,data){
+	await DB.section.find({course_id : course_ID},function(err,data){
 		for(i in data){
 			var section_ID = data[i]._id;
 			DB.lesson.remove({section_id : section_ID},function(err,data){
@@ -98,19 +89,18 @@ router.delete('/delete/:course_id',function(req,res,next)
 			})
 		}
 	})
-	.then(function(){
-		DB.course.remove({_id : course_ID},function(err,data){
-		
-		})
-		res.json(null);
-	});
+	DB.course.remove({_id : course_ID},function(err,data){
+	
+	})
+	res.json(null);
+
 });
 
-router.delete('/deleteSection/:section_id',function(req,res,next)
+router.delete('/deleteSection/:section_id',async function(req,res,next)
 {
 	var section_ID = req.params.section_id;
 	var s_duration = 0;
-	DB.lesson.find({section_id : section_ID},function(err,data){
+	await DB.lesson.find({section_id : section_ID},function(err,data){
 		for(i in data){
 			s_duration += data[i].duration;
 			DB.lesson.remove({_id : data[i]._id},function(err,data){
@@ -118,40 +108,36 @@ router.delete('/deleteSection/:section_id',function(req,res,next)
 			})
 		}
 	})
-	.then(function(){
-		var course_ID;
-		DB.section.find({_id : section_ID},function(err,data){
-			course_ID = data[0].course_id;
-			DB.section.remove({_id : data[0]._id},function(err,data){
-				update_c_duration([-s_duration , course_ID])
-				res.json(null);
-			})
+	
+	var course_ID;
+	DB.section.find({_id : section_ID},function(err,data){
+		course_ID = data[0].course_id;
+		DB.section.remove({_id : data[0]._id},function(err,data){
+			update_c_duration([-s_duration , course_ID])
+			res.json(null);
 		})
-		
-	});
+	})
 });
 
-router.delete('/deleteLesson/:lesson_id',function(req,res,next)
+router.delete('/deleteLesson/:lesson_id',async function(req,res,next)
 {
 	var lesson_ID = req.params.lesson_id;
 	var section_ID;
 	var duration = 0;
-	DB.lesson.find({_id : lesson_ID},function(err,data){
+	await DB.lesson.find({_id : lesson_ID},function(err,data){
 		duration = data[0].duration;
 		section_ID = data[0].section_id;
 		DB.lesson.remove({_id : data[0]._id},function(err,data){
 			
 		})
 	})
-	.then(function(){
-		var course_ID;
-		DB.section.find({_id : section_ID},function(err,data){
-			course_ID = data[0].course_id;
-			update_s_duration([-duration , section_ID])
-			update_c_duration([-duration , course_ID])
-			res.json(null);
-		})
-	});
+	var course_ID;
+	DB.section.find({_id : section_ID},function(err,data){
+		course_ID = data[0].course_id;
+		update_s_duration([-duration , section_ID])
+		update_c_duration([-duration , course_ID])
+		res.json(null);
+	})
 });
 
 
@@ -159,11 +145,12 @@ module.exports = router;
 
 let update_s_duration = (arr) => {
     return new Promise(
-        (resolve) => {
+        (resolve, reject) => {
         	s_duration = arr[0];
         	section_id = arr[1];
         	DB.section.findByIdAndUpdate(section_id,{$inc: {duration : s_duration}},function (err, data) 
 			{
+				if(err) return reject(err);
 				resolve([s_duration]);
     		});
     		
@@ -173,12 +160,12 @@ let update_s_duration = (arr) => {
 
 let update_c_duration = (arr) => {
     return new Promise(
-        (resolve) => {
+        (resolve, reject) => {
         	c_duration = arr[0];
         	course_id = arr[1];
         	DB.course.findByIdAndUpdate(course_id,{$inc: {duration : c_duration}},function (err, data) 
 			{
-	    		//if (err) return next(err);
+	    		if (err) return reject(err);
 	    	});
     		
         }
@@ -190,7 +177,7 @@ var count1;
 
 let create_section = (sections,course_ID,i) => {
     return new Promise(
-        (resolve) => {
+        (resolve , reject) => {
         	var s_duration = 0;
 			var sname = sections[i].name;
 			var s_obj = {
@@ -200,19 +187,19 @@ let create_section = (sections,course_ID,i) => {
 				course_id : course_ID,
 			}
 			var section_id;
-			DB.section.create(s_obj,function (err, data_s) 
-			{
-				if (err) return next(err);
-				section_id = data_s._id;
-				var lesson = sections[i].lessons;
-				add_lessons(lesson,section_id,i)
-				.then(function(arr){
-					resolve(arr);
-				})
-				.catch(err => {
-	    			console.log(err)
-				});;
-			});
+			DB.section.create(s_obj,
+				async (err, data_s) => {
+					try{
+						//if (err) return next(err);
+						section_id = data_s._id;
+						var lesson = sections[i].lessons;
+						var arr = await add_lessons(lesson,section_id,i)
+						resolve(arr);
+					}
+					catch(e){
+						reject(e);
+					}
+				});
 			
         }
     );
@@ -220,36 +207,34 @@ let create_section = (sections,course_ID,i) => {
 
 let add_sections = (sections,course_id) => {
     return new Promise(
-        (resolve) => {
-        	var c_duration=0;
-        	count1=0;
-            for(i in sections)
-			{
-				var flag = false;
-				create_section(sections,course_id,i)
-				//ss.then(update_s_duration)
-				.then(function(arr){
+        async (resolve, reject) => {
+        	try{
+        		var c_duration=0;
+	        	count1=0;
+	            for(i in sections)
+				{
+					var flag = false;
+					console.log("in "+i);
+					var arr = await create_section(sections,course_id,i);
 					c_duration += arr[0];
 					count1++;
 					if(count1 == sections.length){
 		        		flag = true;
 		        	}
-					if(flag)
+		        	if(flag){
 						update_c_duration([c_duration,course_id]);
-				})
-				.catch(err => {
-	    			console.log(err)
-				});
-				//c_duration += s_duration;
-			}
-			
+					}
+				}
+			} catch (e) {
+		        // This will catch exceptions from DoSomethingThatThrows
+		    }	
         }
     );
 };
 
 let create_lesson = (lesson,section_ID,j,i) => {
     return new Promise(
-        (resolve) => {
+        (resolve , reject) => {
             var flag=false;
             var l_obj = {
                 name : lesson[j].name,
@@ -259,7 +244,7 @@ let create_lesson = (lesson,section_ID,j,i) => {
             };
             DB.lesson.create(l_obj,function(err , data_l)
             {
-                if(err) return next(err);
+                //if(err) return reject(err);
                 count[i]++;
 	            if(count[i] == lesson.length)
 	            {
@@ -273,31 +258,23 @@ let create_lesson = (lesson,section_ID,j,i) => {
 
 let add_lessons = (lesson,section_ID,i) => {
     return new Promise(
-        (resolve) => {
-        	var j;
-        	count[i]=0;
-			var s_duration=0;
-            for(j in lesson)
-			{
-				create_lesson(lesson,section_ID,j,i).
-                then(function(arr)
-                {
-                    s_duration+=arr[0];
-                    if(arr[1])
-                    {
-                        update_s_duration([s_duration,section_ID]).
-                        then(function(arr){
-                        	resolve([arr[0]]);
-                        });
-                        ;
-                    }
-                })
-                .catch(err => {
-	    			console.log(err)
-				});;
-				//c_duration += s_duration;
-			}
-			
-        }
-    );
+        async (resolve, reject) => {
+        	try{
+        		var j;
+	        	count[i]=0;
+				var s_duration=0;
+	            for(j in lesson)
+				{
+					var arr = await create_lesson(lesson,section_ID,j,i)
+	          	    s_duration+=arr[0];
+	          	    if(arr[1])
+	                {
+	                    var arr = await update_s_duration([s_duration,section_ID]);
+	                    resolve([arr[0]]);
+	                }
+	            }
+	        } catch (e) {
+		       console.log(e);
+		    }
+        });
 };
